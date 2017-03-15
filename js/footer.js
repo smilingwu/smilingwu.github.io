f821e24a6601022b152a16d346e360c4d0d937ed
smilingwu <457503106@qq.com>
2017-03-15 19:02:10 +0800

    updata file

---
+++ js/footer.js
@@ -0,0 +1,84 @@
+var footer=''+
+'<div class="container-fluid" id="footer">'+
+    '<div class="container">'+
+        '<div class="rol">'+
+            '<div class="col-sm-12 col-md-2 manual">'+
+                '<h4><i class="icon-book"></i>&ensp;在线手册</h4>'+
+                '<ul class="list-unstyled">'+
+                    '<li><a href="http://www.w3cfuns.com/tools.php?mod=bookhtml5">HTML5 参考手册</a></li>'+
+                    '<li><a href="http://www.w3cfuns.com/tools.php?mod=bookcss">CSS 参考手册</a></li>'+
+                    '<li><a href="http://www.w3cfuns.com/tools.php?mod=bookjs">JavaScriot 参考手册</a></li>'+
+                    '<li><a href="http://www.w3cfuns.com/tools.php?mod=bookjquery">JQuery 参考手册</a></li>'+
+                '</ul>'+
+            '</div>'+
+            '<div class="col-sm-12 col-md-6 Tlink fl">'+
+                '<h4><i class="icon-link"></i>&ensp;技术快链</h4>'+
+                '<a href="http://jquery.com/"><i class="icon-tag"></i> jQuery</a>'+
+                '<a href="http://jquerymobile.com/"><i class="icon-tag"></i> jQuery Mobile</a>'+
+                '<a href="http://jqueryui.com/"><i class="icon-tag"></i> jQuery UI</a>'+
+                '<a href="http://www.bootcss.com/"><i class="icon-tag"></i> Bootstrap</a>'+
+                '<a href="http://www.bootcss.com/p/font-awesome/"><i class="icon-tag"></i> Font Awesome</a>'+
+                '<a href="http://www.lesscss.net/"><i class="icon-tag"></i> Less</a>'+
+                '<a href="http://sae.sina.com.cn/"><i class="icon-tag"></i> Sina SAE</a>'+
+                '<a href="http://nodejs.org/"><i class="icon-tag"></i> NodeJS</a>'+
+                '<a href="http://angularjs.cn/"><i class="icon-tag"></i> AngularJS</a>'+
+                '<a href="http://requirejs.org/"><i class="icon-tag"></i> RequireJS</a>'+
+                '<a href="http://www.gruntjs.net/"><i class="icon-tag"></i> Grunt</a>'+
+                '<a href="http://www.bootcss.com/p/chart.js/"><i class="icon-tag"></i> ChartJS</a>'+
+                '<a href="http://snapsvg.io/"><i class="icon-tag"></i> Snap.svg</a>'+
+                '<a href="http://git-scm.com/"><i class="icon-tag"></i> Git</a>'+
+                '<a href="http://ionicframework.com/"><i class="icon-tag"></i> Ionic</a>'+
+            '</div>'+
+            '<div class="col-sm-12 col-md-4 fl">'+
+                '<h4><i class="icon-thumbs-up-alt"></i>&ensp;酷站推荐</h4>'+
+                '<ul class="list-unstyled member">'+
+                    '<li>'+
+                        '<a href="http://www.w3school.com.cn/">'+
+                            '<img src="images/w3c.jpg" class="img-circle" alt="w3cschool" title="w3cschool">'+
+                        '</a>'+
+                    '</li>'+
+                    '<li>'+
+                        '<a href="https://github.com/">'+
+                            '<img src="images/github.jpg" class="img-circle" alt="github" title="github">'+
+                        '</a>'+
+                    '</li>'+
+                    '<li>'+
+                        '<a href="http://codepen.io/">'+
+                            '<img src="images/codepen.jpg" class="img-circle" alt="code'+
+                            'pen" title="codepen">'+
+                        '</a>'+
+                    '</li>'+
+                    '<li>'+
+                        '<a href="https://dribbble.com/">'+
+                            '<img src="images/dribbble.jpg" class="img-circle" alt="dribbble" title="dribbble">'+
+                        '</a>'+
+                    '</li>'+
+                    '<li>'+
+                        '<a href="http://www.68design.net/">'+
+                            '<img src="images/68design.jpg" class="img-circle" alt="网页设计师联盟" title="网页设计师联盟">'+
+                        '</a>'+
+                    '</li>'+
+                '</ul>'+
+            '</div>'+
+        '</div>'+
+    '</div>'+
+'</div>'+
+'<div class="container-fluid" id="copyRight">'+
+    '<div class="container">'+
+        '<div class="rol">'+
+            '<div class="col-xs-12 col-sm-6 copyright">'+
+                '<p class="">Copyright © 2017.Wu.com All rights reserved.</p>'+
+            '</div>'+
+            '<div class="col-xs-12 col-sm-6">'+
+                '<ul class="list-unstyled">'+
+                    '<li><a href="index.html">Home</a></li>'+
+                    '<li><a href="web.html">Project</a></li>'+
+                    '<li><a href="about.html">About</a></li>'+
+                    '<li><a href="contact.html">Contact</a></li>'+
+                '</ul>'+
+            '</div>'+
+        '</div>'+
+    '</div>'+
+'</div>'+
+'<div id="scrolltotop"></div>';
+document.write(footer);
---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(function(){
+    var $navbarBtn=$('.navbar-toggle');
+    $navbarBtn.click(function() {
+        $('#navbar-collapse').toggle(400);
+    });
+})
